cf93e5f40f51f58095e0225a24edd8369dbed769
Oren Mittman <nihil2501@gmail.com>
2023-03-01 18:43:43 -0500

    generate pdf from html using puppeteer and local chrome

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import data from './data.js'
 import Mustache from 'mustache'
-import { readFileSync, writeFileSync } from 'node:fs'
+import { readFileSync, writeFileSync, realpathSync } from 'node:fs'
+import puppeteer from 'puppeteer-core';
 
 const template = readFileSync('template.mustache', 'utf8');
 const rendered = Mustache.render(template, data);
 
-writeFileSync('resume.html', rendered);
+const htmlFileName = 'resume.html';
+writeFileSync(htmlFileName, rendered);
+
+const browser = await puppeteer.launch({
+  executablePath: "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+});
+
+const page = await browser.newPage();
+await page.goto(`file://${realpathSync(htmlFileName)}`);
+await page.pdf({path: 'resume.pdf'});
+await browser.close();
